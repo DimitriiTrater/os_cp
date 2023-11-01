56e1d3b4fc86fc14ea66c0a1231802aafc46f7fd
DimitriiTrater <dima1603040@gmail.com>
2023-11-01 20:01:16 +0200

    string files

---
+++ src/impl/x86_64/string.c
@@ -0,0 +1,9 @@
+#include "string.h"
+
+uint32_t strlen(const_string str) {
+  uint32_t len = 0;
+  while (str[len]) {
+    len++;
+  }
+  return len;
+}
---
+++ src/intf/string.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "types.h"
+
+typedef char *mut_string;
+typedef const char *const_string;
+
+uint32_t strlen(const_string str);
